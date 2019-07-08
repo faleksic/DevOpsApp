faa9060f2a78a539842e628fd8f4e2c80565656c
Filip Aleksić <filipp.aleksic@gmail.com>
2019-07-08 14:46:54 +0200

    Add edit text widget and display content on next screen

--- app/src/main/java/hr/faleksic/devopsapp/MainActivity.java
+++ app/src/main/java/hr/faleksic/devopsapp/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "hr.faleksic.devopsapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void showNewScreen(View view) {
         Intent intent = new Intent(this, NewScreenActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
--- app/src/main/java/hr/faleksic/devopsapp/NewScreenActivity.java
+++ app/src/main/java/hr/faleksic/devopsapp/NewScreenActivity.java
@@ -1,7 +1,9 @@
 package hr.faleksic.devopsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NewScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class NewScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_screen);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
