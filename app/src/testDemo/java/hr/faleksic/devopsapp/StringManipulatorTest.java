00d3a1a9ec24fcbfebcfe27ea7c53a7b4938a1d4
Filip Aleksić <filipp.aleksic@gmail.com>
2019-07-15 15:22:17 +0200

    Add unit test for different flavors

---
+++ app/src/testDemo/java/hr/faleksic/devopsapp/StringManipulatorTest.java
@@ -0,0 +1,12 @@
+package hr.faleksic.devopsapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringManipulatorTest {
+    @Test
+    public void validateConvertToReverseUpperCase() {
+        assertEquals("THIS IS A TEST", StringManipulator.convertToReverseUpperCase("this is a test"));
+    }
+}
